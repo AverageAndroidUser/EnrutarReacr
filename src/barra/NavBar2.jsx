7b6e2e356959aa53df3564b0effbe46820f873b4
AverageAndroidUser <juandiegoascanio13@gmail.com>
2025-06-28 07:59:54 -0500

    profile

--- src/barra/NavBar2.jsx
+++ src/barra/NavBar2.jsx
@@ -4,8 +4,9 @@ export default function NavBar2() {
   return (
     <nav>
       {/*El nombre dentro de to es el nombre de la URL no tiene que ver con el nombre del componente---*/}
-      <Link to="/login">login</Link> |<Link to="/info">info</Link> |
-      <Link to="/profile">profile</Link> |
+      <Link to="/login">login</Link> |
+      <Link to="/info">info</Link> |
+      <Link to="/perfil">profile</Link> |
     </nav>
   );
 }
